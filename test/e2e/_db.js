1611c7287435d8d4db44187aaf113ccfcfcc65c4
Wendy Beck <webeck@gmail.com>
2017-09-18 11:49:25 -0700

    adding auth

--- test/e2e/_db.js
+++ test/e2e/_db.js
@@ -1,5 +1,4 @@
-process.env.MONGODB_URI = 'mongodb://localhost:27017/varcity-test';
-require('../../lib/helpers/connect');
+require('../../lib/helpers/connect')();
 const connection = require('mongoose').connection;
 
 module.exports = {
--- test/e2e/media.test.js
+++ test/e2e/media.test.js
@@ -1,77 +1,89 @@
-
-const db = require('./_db');
+const { drop } = require('./_db');
 const request = require('./_request');
 const assert = require('chai').assert;
 const User = require('../../lib/models/User');
 
-describe('media api', () => {
-
-  before(db.drop);
-
+describe.skip('media api', () => {
   let mediaTestUser = {
     email: 'media@test.com',
     password: 'pword'
   };
 
   let token = '';
+before(drop)
   before(async () => {
-    token = await request.post('/api/auth/signup').send(mediaTestUser).then(res => res.body);
+    token = await request
+      .post('/api/auth/signup')
+      .send(mediaTestUser)
+      .then(res => token = res.body.token);
+    mediaTestUser = await request
+      .post('/api/auth/signin')
+      .set('Authorization', token);
+
+      console.log('mtu: ', mediaTestUser.body);
+      request.getUser.find({ email: 'media@test.com'})
+      .then(result => result);
+      console.log('result: ', result)
+
+    // return;
   });
-  before(async () => mediaTestUser = await User.find({ email: 'media@test.com'}))
 
   let testImg = {
     description: 'testImg description',
     mediaType: 'image upload',
-    img: '/Users/fitzgerald/Documents/CodeFellows/401/varsity-container-folder/varcity-server/test/e2e/icons8-Basketball-64.png'
+    img:
+      '/Users/fitzgerald/Documents/CodeFellows/401/varsity-container-folder/varcity-server/test/e2e/icons8-Basketball-64.png'
   };
 
   let testVideo = {
     description: 'testVideo description',
-    mediaType: 'video link',    
+    mediaType: 'video link',
     videoUrl: 'https://youtu.be/rNRFQ9mtEw4'
   };
 
   let newTestImg = {
     description: 'New testImg description',
     mediaType: 'image upload',
-    img: '/Users/fitzgerald/Documents/CodeFellows/401/varsity-container-folder/varcity-server/test/e2e/icons8-Basketball-64.png'
+    img:
+      '/Users/fitzgerald/Documents/CodeFellows/401/varsity-container-folder/varcity-server/test/e2e/icons8-Basketball-64.png'
   };
 
   let newTestVideo = {
     description: 'New testVideo description',
-    mediaType: 'video link',    
+    mediaType: 'video link',
     videoUrl: 'https://youtu.be/rNRFQ9mtEw4'
   };
 
   function saveVideo(video) {
     const userId = mediaTestUser[0]._id;
     return request
-    .post(`/api/athletes/${userId}/media`)
-    .set('Authorization', token)
-    .send(video)
-    .then(res => {
-      let body = res.body;
-      return video;
-    });
+      .post(`/api/athletes/${userId}/media`)
+      .set('Authorization', token)
+      .send(video)
+      .then(res => {
+        let body = res.body;
+        return video;
+      });
   }
 
   function saveImage(image) {
     const userId = mediaTestUser[0]._id;
     return request
-    .post(`/api/athletes/${userId}/media`)
-    .set('Authorization', token)
-    .field('description', image.description)
-    .field('mediaType', image.mediaType)
-    .attach('image', image.img)
-    .then(res => {
-      let body = res.body;
-      return image;
-    });
+      .post(`/api/athletes/${userId}/media`)
+      .set('Authorization', token)
+      .field('description', image.description)
+      .field('mediaType', image.mediaType)
+      .attach('image', image.img)
+      .then(res => {
+        let body = res.body;
+        return image;
+      });
   }
 
   it('Initial /GET returns empty list', () => {
     const userId = mediaTestUser[0]._id;
-    return request.get(`/api/athletes/${userId}/media`)
+    return request
+      .get(`/api/athletes/${userId}/media`)
       .set('Authorization', token)
       .then(req => {
         const media = req.body;
@@ -80,46 +92,46 @@ describe('media api', () => {
   });
 
   it('saves a video', () => {
-    return saveVideo(testVideo)
-    .then(saved => assert.deepEqual(saved, testVideo));
+    return saveVideo(testVideo).then(saved =>
+      assert.deepEqual(saved, testVideo)
+    );
   });
-  
+
   it('saves an image', () => {
-    return saveImage(testImg)
-      .then(saved => assert.deepEqual(saved, testImg));
+    return saveImage(testImg).then(saved => assert.deepEqual(saved, testImg));
   });
 
   it('Gets all media', () => {
-    return Promise.all([
-      saveImage(newTestImg),
-      saveVideo(newTestVideo)
-    ])
+    return Promise.all([saveImage(newTestImg), saveVideo(newTestVideo)])
       .then(savedMedia => {
         newTestImg = savedMedia[0];
         newTestVideo = savedMedia[1];
       })
-      .then(() => request.get('/api/athletes/:id/media')
-        .set('Authorization', token)
-        .then(res => res.body)
-        .then(media => {
-          assert.deepEqual(media[1].description, testImg.description);
-          assert.deepEqual(media[2], newTestVideo);
-        })
-    );
+      .then(() =>
+        request
+          .get('/api/athletes/:id/media')
+          .set('Authorization', token)
+          .then(res => res.body)
+          .then(media => {
+            assert.deepEqual(media[1].description, testImg.description);
+            assert.deepEqual(media[2], newTestVideo);
+          })
+      );
   });
 
-
   xit('patches a media', () => {
-    const userId = mediaTestUser[0]._id;    
-    return request.patch(url)
+    const userId = mediaTestUser[0]._id;
+    return request
+      .patch(url)
       .send({ genus: 'something else' })
       .then(res => res.body)
       .then(res => assert.deepEqual(res.genus, 'something else'));
   });
 
   xit('deletes a piece of media', () => {
-    const userId = mediaTestUser[0]._id;    
-    return request.delete(url)
+    const userId = mediaTestUser[0]._id;
+    return request
+      .delete(url)
       .then(res => {
         assert.deepEqual(res.body, { removed: true });
         return res;
