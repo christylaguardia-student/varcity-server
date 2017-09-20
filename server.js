966853669b12b03cb04ad72ae7b5727f24fa5462
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-09-20 11:03:31 -0700

    Tests use test db, otherwise we use varcity db

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 /* eslint no-console: "off" */
-require('dotenv').config()
+require('dotenv').config();
 const app = require('./lib/app');
-const connect = require('./lib/helpers/connect');
 const http = require('http');
-const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/varcity';
+require('./lib/helpers/connect');
+
 const port = process.env.PORT || 3001;
 const server = http.createServer(app);
-connect(dbUri);
 
 server.listen(port, () => {
-    console.log('server running on', server.address());
+  console.log('server running on', server.address());
 });
