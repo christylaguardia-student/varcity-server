7f1f3f128082005f4840407fa2d542a708c5625a
Wendy Beck <webeck@gmail.com>
2017-09-20 13:06:38 -0700

    checking for token on refresh

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 require('dotenv').config();
 const app = require('./lib/app');
 const http = require('http');
-require('./lib/helpers/connect');
+require('./lib/helpers/connect')(process.env.MONGODB_URI ||'mongodb://localhost:27017/varcity');
+const connection = require('mongoose').connection;
 
 const port = process.env.PORT || 3001;
 const server = http.createServer(app);
