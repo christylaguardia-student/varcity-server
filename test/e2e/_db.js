053d614f5900c6a01261dbf07fc15ee7c9e639cc
Pierre Salumu-Shabani <jrsalshab@outlook.com>
2017-09-21 08:58:17 -0700

    new commit to get sports up to dev

--- test/e2e/_db.js
+++ test/e2e/_db.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-process.env.MONGODB_URI = 'mongodb://localhost:27017/varcity-test';
-require('../../lib/connect');
-=======
 const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/varcity-test';
 require('../../lib/helpers/connect')(dbUri);
->>>>>>> 966853669b12b03cb04ad72ae7b5727f24fa5462
 const connection = require('mongoose').connection;
 
 module.exports = {
--- test/unit/edu.test.js
+++ test/unit/edu.test.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-const Edu = require('../../lib/models/edu');
-const { assert } = require('chai');
-
-describe('Edu model', () => {
-=======
 const Edu = require('../../lib/models/Edu');
 const { assert } = require('chai');
 
 describe.skip('Edu model', () => {
->>>>>>> 966853669b12b03cb04ad72ae7b5727f24fa5462
   it('validates required fields', () => {
     const edu = new Edu({
       institution: 'Harvard'
@@ -18,10 +11,6 @@ describe.skip('Edu model', () => {
 
   it('fails validation when required fields are missing', () => {
     const edu = new Edu();
-<<<<<<< HEAD
-    
-=======
->>>>>>> 966853669b12b03cb04ad72ae7b5727f24fa5462
     return edu.validate()
       .then( () => { throw new Error('Expected validation error');
       },
@@ -29,8 +18,4 @@ describe.skip('Edu model', () => {
         assert.ok(errors.institution);
       });
   });
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 966853669b12b03cb04ad72ae7b5727f24fa5462
