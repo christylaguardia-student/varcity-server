a05c5a9862c97e8a9c2314995b2ab6d7cf1aa75d
Wendy Beck <webeck@gmail.com>
2017-09-22 02:58:12 -0700

    working on search

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = require('./lib/app');
 const http = require('http');
 
 const connect = require('./lib/helpers/connect');
-const dbUri = process.env.MONGODB_URI || "mongodb://maneki-neko:maneki-neko@ds135594.mlab.com:35594/heroku_lwzqtf4h";
+// const dbUri = process.env.MONGODB_URI || "mongodb://maneki-neko:maneki-neko@ds135594.mlab.com:35594/heroku_lwzqtf4h";
+const dbUri = process.env.MONGODB_URI || "mongodb://localhost:27017/varcity";
 connect(dbUri);
 
 const port = process.env.PORT || 3001;
--- test/e2e/_db.js
+++ test/e2e/_db.js
@@ -1,4 +1,5 @@
-const dbUri = process.env.MONGODB_URI || 'mongodb://maneki-neko:maneki-neko@ds135594.mlab.com:35594/varcity-test';
+// const dbUri = process.env.MONGODB_URI || 'mongodb://maneki-neko:maneki-neko@ds135594.mlab.com:35594/varcity-test';
+const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/varcity'
 require('../../lib/helpers/connect')(dbUri);
 const connection = require('mongoose').connection;
 
