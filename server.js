567674ee840f47382f1f2c80f45605f84fd076f1
Wendy Beck <webeck@gmail.com>
2017-09-21 18:31:01 -0700

    aading search

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = require('./lib/app');
 const http = require('http');
 
 const connect = require('./lib/helpers/connect');
-const dbUri = process.env.MONGODB_URI;
+const dbUri = process.env.MONGODB_URI || "mongodb://localhost:27017/varcity";
 connect(dbUri);
 
 const port = process.env.PORT || 3001;
