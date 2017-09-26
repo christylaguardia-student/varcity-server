030d765d7a0952092e8c3a52d3208352c92d5892
christylaguardia <christinelaguardia@gmail.com>
2017-09-26 10:29:49 -0700

    pulled in Pierre's sport files (couldn't merge)

---
+++ test/unit/sport.test.js
@@ -0,0 +1,25 @@
+const Sport = require('../../lib/models/sport');
+const { assert } = require('chai');
+
+describe('Sport model', () => {
+  it('validates required fields', () => {
+    const sport = new Sport({
+      sport: 'Basketball',
+      organization: 'OSU',
+      position: 'Shooting Guard'
+    });
+    return sport.validate();
+  });
+
+  it('fails validation when required fields are missing', () => {
+    const sport = new Sport();
+    return sport.validate()
+      .then( () => { throw new Error('Expected validation error');
+      },
+      ({ errors }) => {
+        assert.ok(errors.sport);
+        assert.ok(errors.organization);
+        assert.ok(errors.position);
+      });
+  });
+});
